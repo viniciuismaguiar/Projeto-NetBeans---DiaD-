7d31bf453f853343c5c1d69392e14c3e0aeea1b4
Vinícius <viniciuis.m.aguiar2022@gmail.com>
2026-04-24 16:21:53 -0300

    Arquivos do ProjetoX

---
+++ src/Controlador/Controlador2.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+
+/**
+ *
+ * @author vinic
+ */
+public class Controlador2 {
+    JButton jButton1;
+    JButton jButton2;
+    JButton jButton3;
+    JButton jButton4;
+    JButton jButton5;
+    JLabel jLabel1;
+    JLabel jLabel2;
+    JLabel jLabel3;
+    JLabel jLabel4;
+    JLabel jLabel5;
+    JLabel jLabel6;
+    
+
+    public Controlador2(JButton jButton1, JButton jButton2, JButton jButton3, JButton jButton4, JButton jButton5, JLabel jLabel1, JLabel jLabel2, JLabel jLabel3, JLabel jLabel4, JLabel jLabel5, JLabel jLabel6) {
+        this.jButton1 = jButton1;
+        this.jButton2 = jButton2;
+        this.jButton3 = jButton3;
+        this.jButton4 = jButton4;
+        this.jButton5 = jButton5;
+        this.jLabel1 = jLabel1;
+        this.jLabel2 = jLabel2;
+        this.jLabel3 = jLabel3;
+        this.jLabel4 = jLabel4;
+        this.jLabel5 = jLabel5;
+        this.jLabel6 = jLabel6;
+    }
+
+    
+    
+    public void actionFeminicidio() {
+      this.jLabel1.setText("<html> <div style = 'width:190px;'>Feminicídio");
+      this.jLabel2.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'><b>Igualdade de direitos</b><></span>" 
+        + "<ul> O feminismo busca garantir que mulheres tenham os mesmos direitos políticos, sociais e econômicos que os homens, defendendo oportunidades justas na educação, no trabalho e na participação política.<li>");
+      this.jLabel3.setText("<html> <div style = 'width:500px;'>" + "<span style= 'font-size:11px;'><b>Combate à desigualdade de gênero</b></span>"
+        + "<ul> O movimento questiona estruturas sociais que colocam mulheres em posição de desvantagem, incentivando mudanças culturais e sociais para reduzir preconceitos e desigualdades históricas. <li>");
+      this.jLabel4.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>História de lutas sociais</b></span>"
+        + "<ul>Ao longo do tempo, o feminismo esteve ligado a conquistas como o direito ao voto, acesso à educação superior e maior participação das mulheres no mercado de trabalho e na vida pública.<li>");
+      this.jLabel5.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>Diversidade de correntes</b></span>"
+        + "<ul>Existem diferentes vertentes feministas, que interpretam e defendem a igualdade de maneiras distintas, analisando fatores sociais, culturais e econômicos que influenciam a vida das mulheres.<li>");
+      this.jLabel6.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>Debates contemporâneo</b></span>"
+        + "<ul>Atualmente o feminismo discute temas como representatividade, direitos no trabalho, igualdade salarial e o combate à violência de gênero em diferentes contextos da sociedade.<li>");
+    }
+
+    
+    public void actionMisoginia(){
+        this.jLabel1.setText("<html> <div style = 'width:190px;'>Misoginia");
+        this.jLabel2.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'><b>Preconceito contra mulheres</b><></span>" 
+        + "<ul> A misoginia refere-se ao desprezo, aversão ou preconceito direcionado às mulheres, manifestando-se por meio de atitudes, discursos ou comportamentos discriminatórios que reforçam estereótipos negativos.<li>");
+        this.jLabel3.setText("<html> <div style = 'width:500px;'>" + "<span style= 'font-size:11px;'><b>Raízes culturais e históricas</b></span>"
+        + "<ul> Em muitas sociedades, ideias antigas sobre papéis de gênero contribuíram para a formação de visões negativas sobre as mulheres, influenciando costumes e comportamentos sociais. <li>");
+        this.jLabel4.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>Manifestação social</b></span>"
+        + "<ul>Pode aparecer em piadas ofensivas, comentários depreciativos, atitudes de desvalorização ou na negação da capacidade e autonomia feminina.<li>");
+        this.jLabel5.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>Impacto na vida das mulheres</b></span>"
+        + "<ul>A misoginia pode gerar exclusão social, dificuldades profissionais, insegurança e reforçar desigualdades presentes na sociedade.<li>");
+        this.jLabel6.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>Combate ao preconceito</b></span>"
+        + "<ul>A educação, o debate social, a conscientização e políticas de igualdade são caminhos importantes para reduzir atitudes misóginas.<li>"); 
+    }
+
+    public void actionDesigualdadeDeGenero(){
+        this.jLabel1.setText("<html> <div style = 'width:400px;'>Desigualdade de Gênero");
+        this.jLabel2.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'><b>Diferença de oportunidades</b><></span>" 
+        + "<ul>A desigualdade de gênero ocorre quando homens e mulheres não têm as mesmas oportunidades em áreas como trabalho, educação, participação política e acesso a recursos sociais.<li>");
+        this.jLabel3.setText("<html> <div style = 'width:500px;'>" + "<span style= 'font-size:11px;'><b>Questão social histórica</b></span>"
+        + "<ul>Em muitas sociedades, estruturas tradicionais limitaram o acesso das mulheres a certos direitos e posições, criando desigualdades ao longo do tempo.<li>");
+        this.jLabel4.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>Desigualdade no mercado de trabalho</b></span>"
+        + "<ul>Um exemplo comum é a diferença salarial, a menor presença de mulheres em cargos de liderança ou oportunidades limitadas de crescimento profissional.<li>");
+        this.jLabel5.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>Impacto social</b></span>"
+        + "<ul>Essas desigualdades podem afetar a independência financeira, a participação social e o desenvolvimento profissional das mulheres.<li>");
+        this.jLabel6.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>Busca por equidade</b></span>"
+        + "<ul>Diversas iniciativas sociais, educacionais e políticas procuram reduzir essas diferenças e promover igualdade de oportunidades.<li>");
+}
+
+    public void actionMachismo(){
+        this.jLabel1.setText("<html> <div style = 'width:190px;'>Machismo");
+        this.jLabel2.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'><b>Ideia de superioridade masculina</b><></span>" 
+        + "<ul>O machismo é uma visão cultural que coloca os homens em posição de superioridade em relação às mulheres, influenciando atitudes e comportamentos sociais.<li>");
+        this.jLabel3.setText("<html> <div style = 'width:500px;'>" + "<span style= 'font-size:11px;'><b>Influência cultural</b></span>"
+        + "<ul>Em muitos contextos históricos e sociais, comportamentos machistas foram normalizados ou incentivados por tradições e costumes culturais.<li>");
+        this.jLabel4.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>Comportamentos cotidianos</b></span>"
+        + "<ul>Pode aparecer em atitudes como desvalorizar opiniões femininas, limitar escolhas ou esperar que mulheres cumpram apenas certos papéis sociais.<li>");
+        this.jLabel5.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>Consequências sociais</b></span>"
+        + "<ul>Esse tipo de pensamento contribui para desigualdades, conflitos sociais e limita a liberdade e autonomia das mulheres.<li>");
+        this.jLabel6.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>Mudança cultura</b></span>"
+        + "<ul>A conscientização social, a educação e o respeito mútuo são importantes para reduzir atitudes machistas e promover relações mais equilibradas.<li>");
+    }
+
+    public void actionAssedio(){
+        this.jLabel1.setText("<html> <div style = 'width:190px;'>Assédio");
+          this.jLabel2.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'><b>Comportamento invasivo</b><></span>" 
+        + "<ul>O assédio envolve atitudes insistentes, ofensivas ou constrangedoras dirigidas a outra pessoa sem seu consentimento, causando desconforto ou intimidação.<li>");
+        this.jLabel3.setText("<html> <div style = 'width:500px;'>" + "<span style= 'font-size:11px;'><b>Assédio no ambiente de trabalho</b></span>"
+        + "<ul>Pode ocorrer por meio de comentários inadequados, pressão psicológica ou comportamentos que criam um ambiente de trabalho hostil.<li>");
+        this.jLabel4.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>Assédio verbal ou físico</b></span>"
+        + "<ul>Pode acontecer através de palavras, gestos, mensagens ou contato físico indesejado que cause constrangimento à vítima.<li>");
+        this.jLabel5.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>Impactos psicológicos</b></span>"
+        + "<ul>O assédio pode causar medo, ansiedade, insegurança e diversos prejuízos emocionais e sociais para quem sofre esse tipo de situação.<li>");
+        this.jLabel6.setText("<html> <div style = 'width:500px;'>" + "<span style='font-size:11px;'<b>Importância da denúncia</b></span>"
+        + "<ul>Leis, instituições e políticas públicas buscam proteger vítimas e incentivar a denúncia para combater esse tipo de comportamento.<li>");
+    }
+    
+    
+}
